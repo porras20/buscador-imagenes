95dd74dc37d0b2276b4e0b03c035c91538446a93
Juan Jose Porras <juanjoseporras2019@gmail.com>
2023-03-18 12:42:37 -0500

    Se realizo la busqueda por foto

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,38 @@ import Cards from './components/Cards'
 import Formulario from './components/Formulario'
 
 function App() {
-
+  const [search, setSearch] = useState('');
   const [datos, setDatos] = useState([]);
   const [url, setUrl] = useState('https://api.unsplash.com/photos/?client_id=fb-RZ5I90TondzzVbKvLKNMia-aJaqIpujJ4XB0QC6o')
+  
+
   useEffect(() => {
-    const consultarApi = async () =>{
-      const result = await axios.get(url)
-      setDatos(result.data);
+
+    if (!search) {
+      const consultarApi = async () =>{
+        const result = await axios.get(url);
+        const resultado = result.data;
+        setDatos(resultado);
+      }
+      consultarApi()
     }
-    consultarApi()
-  }, [])
+    else{
+        const consultarApi2 = async () =>{
+          const result = await axios.get(`https://api.unsplash.com/search/photos/?client_id=fb-RZ5I90TondzzVbKvLKNMia-aJaqIpujJ4XB0QC6o&query=${search}`)
+          const resultado = result.data.results;
+          setDatos(resultado)
+        }
+        consultarApi2()
+      }
+  }, [search])
+
+  const busqueda = (busqueda) =>{
+    setSearch(busqueda);
+  }
   
   return (
     <>
-      <Formulario />
+      <Formulario busqueda={busqueda}/>
       <Cards datos={datos}/>
     </>
   )
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,15 @@
-const Formulario = () => {
+import { useState } from "react";
+
+const Formulario = ({busqueda}) => {
+    const guardarBusqueda = e =>{
+        busqueda(e.target.value)
+    }
     return ( 
         <div className="container-form">
             <div className="form">
                 <h2>Busca tu imagen</h2>
                 <p>Busca cualquier imagen libre de derechos de autor</p>
-                <input type="text" placeholder="Busca una imagen" />
+                <input type="text" placeholder="Busca una imagen" onChange={guardarBusqueda}/>
             </div>
         </div>
      );
